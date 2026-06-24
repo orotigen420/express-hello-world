cadf845ca901ad9e24118e27dfc3a57093779ebe
orotigen <tomo2708@outlook.jp>
2026-06-24 20:26:53 +0900

    名前つける機能追加

--- app.js
+++ app.js
@@ -7,24 +7,24 @@ expressWs(app)
 const port = process.env.PORT || 3001
 let connects = []
 
-app.use(express.static('public'))
+app.use(express.static('public'))//これによってpublicフォルダから自動的にindex.htmlを探し出す。
 
 app.ws('/ws', (ws, req) => {
   connects.push(ws)
 
-  ws.on('message', (message) => {
-    console.log('Received:', message)
+  ws.on('message', (message) => { //メッセージを受信
+    const msg = JSON.parse(message)
+    console.log('received:', msg)
 
     connects.forEach((socket) => {
-      if (socket.readyState === 1) {
-        // Check if the connection is open
+      if (socket.readyState === 1) {　　//socketが通信可能か確認
         socket.send(message)
       }
     })
   })
 
-  ws.on('close', () => {
-    connects = connects.filter((conn) => conn !== ws)
+  ws.on('close', () => { //接続の切断を受信
+    connects = connects.filter((conn) => conn !== ws) //filterで
   })
 })
 
